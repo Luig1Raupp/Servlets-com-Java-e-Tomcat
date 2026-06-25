b10e3f7998f9d0d9f054208a9d3480777cfca562
Luigi Martins <luigi63466826@alunos.sc.senac.br>
2026-06-25 20:30:44 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/senac/NameWithVariableServlet.java
@@ -0,0 +1,9 @@
+package br.com.senac;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/")
+
+public class NameWithVariableServlet extends HttpServlet {
+}
